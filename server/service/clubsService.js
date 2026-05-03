d623aa86ad733dd73d4421ef48222d21b206bd63
Kris-sys <65308192+Kris-sys@users.noreply.github.com>
2026-05-03 23:54:15 +0300

    boonusülesanne

--- server/service/clubsService.js
+++ server/service/clubsService.js
@@ -2,7 +2,7 @@ const {pool} = require("../database")
 const {SELECT_ALL_CLUBS, SELECT_CLUB_BY_ID, SELECT_TOP_CLUBS, INSERT_CLUB, getAddOrUpdateClubQuery, DELETE_CLUB, SELECT_TOP_PLAYERS_BY_CLUB} = require("./queries");
 
 const getAllClubs = (req, res) => {
-    console.log("IN - Get all clubs request")
+    //console.log("IN - Get all clubs request")
 
     pool.query(SELECT_ALL_CLUBS, (err, results) => {
         if (err) {
@@ -13,14 +13,14 @@ const getAllClubs = (req, res) => {
             })
         }
 
-        console.log("OUT - Get all clubs result: " + JSON.stringify(results.rows))
+        //console.log("OUT - Get all clubs result: " + JSON.stringify(results.rows))
         res.status(200).send(results.rows)
     })
 }
 
 const getClubById = (req, res) => {
     const id = parseInt(req.params.id)
-    console.log(`IN - Get club(id=${id}) request`)
+    //console.log(`IN - Get club(id=${id}) request`)
 
     pool.query(SELECT_CLUB_BY_ID, [id], (err, results) => {
         if (err) {
@@ -31,14 +31,14 @@ const getClubById = (req, res) => {
             })
         }
 
-        console.log(`OUT - Get club(id=${id}) result: ${JSON.stringify(results.rows[0])}`)
+        //console.log(`OUT - Get club(id=${id}) result: ${JSON.stringify(results.rows[0])}`)
         res.status(200).send(results.rows[0])
     })
 }
 
 const getTopClubs = (req, res) => {
     const limit = parseInt(req.params.limit)
-    console.log(`IN - Get top clubs (limit=${limit})`)
+    //console.log(`IN - Get top clubs (limit=${limit})`)
 
     pool.query(SELECT_TOP_CLUBS, [limit], (err, results) => {
         if (err) {
@@ -48,7 +48,7 @@ const getTopClubs = (req, res) => {
                 error: err
             })
         }
-        console.log("OUT - Get top clubs" + JSON.stringify(results.rows))
+        //console.log("OUT - Get top clubs" + JSON.stringify(results.rows))
         res.status(200).send(results.rows)
     })
 }
@@ -70,7 +70,7 @@ const getClubTopPlayers = (req, res) => {
 const addClub = (req, res) => {
     const {name, location, isUpdate, clubId} = req.body
     const update = JSON.parse(isUpdate)
-    console.log(`IN: Add club request: club(name=${name}, location=${location})`)
+    //console.log(`IN: Add club request: club(name=${name}, location=${location})`)
 
     const query = getAddOrUpdateClubQuery(update)
     let values = [name, location]
@@ -87,7 +87,7 @@ const addClub = (req, res) => {
             })
         }
 
-        console.log(`OUT - Add club result: ${JSON.stringify(results.rows[0])}`)
+        //console.log(`OUT - Add club result: ${JSON.stringify(results.rows[0])}`)
         res.status(200).send(results.rows[0])
     })
 }
@@ -105,7 +105,7 @@ const deleteClub = (req, res) => {
             })
         }
 
-        console.log(`OUT - Delete club(id=${clubId}) result: ${JSON.stringify(results.rows)}`)
+        //console.log(`OUT - Delete club(id=${clubId}) result: ${JSON.stringify(results.rows)}`)
         res.status(200).send(results.rows)
     })
 }
