e2f50542c796aa8c113ba4409a0ca56e29ca3240
Kris-sys <65308192+Kris-sys@users.noreply.github.com>
2026-05-03 21:06:29 +0300

    Refactor player and club service imports and routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 /* eslint-env node */
 const express = require('express');
 const cors = require('cors')
-const {getAllPlayers, getPlayerById, getPlayersByClubId, getPlayerStatistics, addPlayer, getTopPlayers, deletePlayer} = require("./service/playersService");
-const {getAllClubs, getClubById, addClub, getTopClubs, deleteClub, getClubTopPlayers} = require("./service/clubsService");
+const {getAllPlayers, getPlayerById, getPlayersByClubId, getPlayerStatistics, addPlayer, getTopPlayers, deletePlayer, getClubTopPlayers } = require("./service/playersService");
+const {getAllClubs, getClubById, addClub, getTopClubs, deleteClub} = require("./service/clubsService");
 const {getAllTournaments, getTournamentById, addTournament, getOngoingTournaments, deleteTournament} = require("./service/tournamentsService");
 const {getMatchByTournamentId, addMatchToTournament, getOngoingMatches, getMatchById} = require("./service/matchesService");
 const {getAllLocations} = require("./service/locationsService");
@@ -22,13 +22,13 @@ app.get("/api/players/:id", getPlayerById)
 app.get("/api/players/club/:id", getPlayersByClubId)
 app.get("/api/players/statistics/:id", getPlayerStatistics)
 app.get("/api/players/top/:limit", getTopPlayers)
+app.get("/api/players/topPlayers/:name", getClubTopPlayers)
 app.post("/api/players", addPlayer)
 app.delete("/api/players/:playerId", deletePlayer)
 
 app.get("/api/clubs", getAllClubs)
 app.get("/api/clubs/:id", getClubById)
 app.get("/api/clubs/top/:limit", getTopClubs)
-app.get("/api/clubs/topPlayers/:name", getClubTopPlayers)
 app.post("/api/clubs", addClub)
 app.delete("/api/clubs/:clubId", deleteClub)
 
